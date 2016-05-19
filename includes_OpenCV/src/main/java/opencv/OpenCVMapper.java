240e3ffd1b5e212d8a26ceac96733617366b2fe8
Veronika Vasilyeva <veronika.qwest@gmail.com>
2016-05-19 19:56:03 +0500

    Fixed opencv mapper test
    Test moved to mipr-open_cv package and fixed.
    OpenCVMapper was modified to allow load opencv from test only.

--- includes_OpenCV/src/main/java/opencv/OpenCVMapper.java
+++ includes_OpenCV/src/main/java/opencv/OpenCVMapper.java
@@ -10,9 +10,18 @@ import java.io.IOException;
  * Created by Epanchee on 13.08.2015.
  */
 public class OpenCVMapper<KEYIN, VALUEIN, KEYOUT, VALUEOUT> extends Mapper<KEYIN, VALUEIN, KEYOUT, VALUEOUT> {
+    private static boolean openCvLoaded = false;
+    public static void set_openCvLoaded(){
+        openCvLoaded = true;
+    }
+
     @Override
     protected void setup(Context context) throws IOException, InterruptedException {
-        Path[] myCacheFiles = DistributedCache.getLocalCacheFiles(context.getConfiguration());
-        System.load(myCacheFiles[0].toUri().getPath());
+        if(!openCvLoaded)
+        {
+            Path[] myCacheFiles = DistributedCache.getLocalCacheFiles(context.getConfiguration());
+            System.load(myCacheFiles[0].toUri().getPath());
+            set_openCvLoaded();
+        }
     }
 }
