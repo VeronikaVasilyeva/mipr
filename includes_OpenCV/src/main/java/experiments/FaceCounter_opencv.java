db30866f76e2749a6eab2331478f3f71c271e0a4
Veronika Vasilyeva <veronika.qwest@gmail.com>
2016-05-22 10:01:10 +0500

    Added face counter job
    Added face counter job. This job detects faces on images and calculate
    count of images with matched count of faces.

---
+++ includes_OpenCV/src/main/java/experiments/FaceCounter_opencv.java
@@ -0,0 +1,76 @@
+package experiments;
+
+import core.MiprConfigurationParser;
+import opencv.MatImageInputFormat;
+import opencv.MatImageOutputFormat;
+import opencv.MatImageWritable;
+import opencv.OpenCVMapper;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.junit.Assert;
+import org.junit.experimental.theories.DataPoint;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
+import org.opencv.objdetect.CascadeClassifier;
+
+import java.io.IOException;
+
+public class FaceCounter_opencv {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        String input = args[0];
+        String output = args[1];
+
+        Job job = new MiprConfigurationParser().getOpenCVJobTemplate();
+        job.setJarByClass(FaceCounter_opencv.class);
+        job.setMapperClass(FaceCounterMapper.class);
+        job.setNumReduceTasks(0);
+        job.setInputFormatClass(MatImageInputFormat.class);
+        job.setOutputFormatClass(MatImageOutputFormat.class);
+        Path outputPath = new Path(output);
+        FileInputFormat.setInputPaths(job, input);
+        FileOutputFormat.setOutputPath(job, outputPath);
+        job.setOutputKeyClass(IntWritable.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.setReducerClass(FaceCounterReducer.class);
+        job.waitForCompletion(true);
+    }
+
+    public static class FaceCounterMapper extends OpenCVMapper<NullWritable, MatImageWritable, IntWritable, IntWritable> {
+
+        @Override
+        protected void map(NullWritable key, MatImageWritable value, Context context) throws IOException, InterruptedException {
+            Mat image = value.getImage();
+
+            //TODO: load image from distributed cache
+            CascadeClassifier faceDetector = new CascadeClassifier("..\\images\\lbpcascade_frontalface.xml");
+            MatOfRect faceDetections = new MatOfRect();
+            faceDetector.detectMultiScale(image, faceDetections);
+
+            int result = faceDetections.toArray().length;
+
+            context.write(new IntWritable(result),new IntWritable(1));
+        }
+    }
+
+    public static class FaceCounterReducer extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable> {
+
+        @Override
+        protected void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable value : values) {
+                sum += value.get();
+            }
+            context.write(key, new IntWritable(sum));
+        }
+    }
+}
+
