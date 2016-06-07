e23f28be77be5e33105799b22d19a495db53f166
Veronika Vasilyeva <veronika.qwest@gmail.com>
2016-06-07 23:21:14 +0500

    build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+wget http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/2.4.9/opencv-2.4.9.zip
+unzip opencv-2.4.9.zip
+cd opencv-2.4.9
+mkdir build
+cd build
+cmake -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON -D WITH_VTK=ON ..
+make -j8
+sudo make -j8 install
+mvn install:install-file -Dfile=/usr/local/share/OpenCV/java/opencv-249.jar -DgroupId=opencv -DartifactId=opencv -Dversion=2.4.9 -Dpackaging=jar
+cd ../..
