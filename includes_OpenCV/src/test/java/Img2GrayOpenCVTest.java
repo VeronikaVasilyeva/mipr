424b3049a5672762c4c2576b85da89251c955b85
VeronikaVasilyeva <veronika.qwest@gmail.com>
2016-06-11 18:56:45 +0500

    add more tests for image to gray

--- includes_OpenCV/src/test/java/Img2GrayOpenCVTest.java
+++ includes_OpenCV/src/test/java/Img2GrayOpenCVTest.java
@@ -1,10 +1,12 @@
 import experiments.Img2Gray_opencv;
 import framework.MiprAssert;
 import opencv.MatImageWritable;
+import opencv.OpenCVMapper;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mrunit.types.Pair;
 import org.junit.Test;
+import org.opencv.core.Core;
 import org.opencv.core.CvType;
 
 import java.util.List;
@@ -14,25 +16,38 @@ import java.util.List;
  */
 public class Img2GrayOpenCVTest {
 
-    @Test
-    public void TestMapper () throws Exception {
-        Mapper<NullWritable, MatImageWritable, NullWritable, MatImageWritable> map = new Img2Gray_opencv.Img2Gray_opencvMapper();
-        MiprOpenCVMapDriver driver = new MiprOpenCVMapDriver<NullWritable, MatImageWritable>(map){};
-        driver.withInputFile("/color.jpg");
-        driver.test(new MiprAssert<NullWritable, MatImageWritable>() {
-            @Override
-            protected void assertResults(List<Pair<NullWritable, MatImageWritable>> runResults){
-                assertEquals(1,runResults.toArray().length);
-            }
+    private MiprAssert<NullWritable, MatImageWritable> _asserts = new MiprAssert<NullWritable, MatImageWritable>() {
+        @Override
+        protected void assertResults(List<Pair<NullWritable, MatImageWritable>> runResults) {
+            assertEquals(1, runResults.toArray().length);
+        }
 
-            @Override
-            protected void assertResult(Pair<NullWritable, MatImageWritable> runResult) {
-                //проверяем, что у изображения один канал (чб)
-                assertEquals(runResult.getSecond().getImage().type(), CvType.CV_8UC1);
-            }
-        });
+        @Override
+        protected void assertResult(Pair<NullWritable, MatImageWritable> runResult) {
+            //проверяем, что у изображения один канал (чб)
+            assertEquals(runResult.getSecond().getImage().type(), CvType.CV_8UC1);
+        }
+    };
+
+    @Test
+    public void TestImage1() throws Exception {
+        TestMapper("/color.jpg");
     }
 
+    @Test
+    public void TestImage2() throws Exception {
+        TestMapper("/10.jpg");
+    }
 
+    @Test
+    public void TestImage3() throws Exception {
+        TestMapper("/face2.jpg");
+    }
 
+    private void TestMapper(String fileName) throws Exception {
+        Mapper<NullWritable, MatImageWritable, NullWritable, MatImageWritable> map = new Img2Gray_opencv.Img2Gray_opencvMapper();
+        MiprOpenCVMapDriver driver = new MiprOpenCVMapDriver<NullWritable, MatImageWritable>(map){};
+        driver.withInputFile(fileName);
+        driver.test(_asserts);
+    }
 }
